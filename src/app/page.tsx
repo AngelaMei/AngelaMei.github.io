a6b69ea0ada7558515360746ef38965d41cdac40
Paul Wang <tzupengwang@gmail.com>
2025-01-07 22:19:09 +0100

    Implement a fixed header Nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,43 @@
 import Image from "next/image";
+import Header from "@/components/header.tsx";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
-      <main className="flex flex-col row-start-2 items-center">
-        <Image
-          src="/eyes.gif"
-          alt="Portfolio logo"
-          width={320}
-          height={38}
-          priority
-        />
-        <div
-            className="flex items-center justify-center text-lg sm:text-3xl h-10 sm:h-12 sm:min-w-44"
-          >
-            🚧 Portfolio Under Construction 🚧
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://meiyuchi.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className="relative flex flex-col mx-4 sm:mx-20">
+      <Header />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
+        <main className="flex flex-col row-start-2 items-center">
           <Image
-            aria-hidden
-            src="/eyes.png"
-            alt="Eyes icon"
-            width={20}
-            height={20}
+            src="/eyes.gif"
+            alt="Portfolio logo"
+            width={320}
+            height={38}
+            priority
           />
-          Go to meiyuchi.com →
-        </a>
-      </footer>
+          <div
+              className="flex items-center justify-center text-lg sm:text-3xl h-10 sm:h-12 sm:min-w-44"
+            >
+              🚧 Portfolio Under Construction 🚧
+          </div>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <a
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+            href="https://meiyuchi.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              aria-hidden
+              src="/eyes.svg"
+              alt="Eyes icon"
+              width={20}
+              height={20}
+            />
+            Go to meiyuchi.com →
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="absolute l-0 t-0 w-full flex items-center text-sm sm:text-2xl gap-4 sm:gap-7 h-24 my-0.5">
+      <div className="flex items-center sm:gap-2">
+        Yu-Chi 
+        <Image
+          aria-hidden
+          src="/eyes.svg"
+          alt="Eyes icon"
+          width={55}
+          height={44}
+        />
+        Mei
+      </div>
+      <div className="flex grow items-center justify-center gap-5">
+        <a className="hidden sm:block" href="mailto:angela101475@gmail.com">
+          <Image
+            src="/Email.svg"
+            alt="email icon"
+            width={28}
+            height={28}
+          />
+        </a>
+        <a
+          className="hidden sm:block"
+          href="https://www.linkedin.com/in/angela-mei"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/LinkedinLogo.svg"
+            alt="Linkedin icon"
+            width={28}
+            height={28}
+          />
+        </a>
+        <a
+          className="hidden sm:block"
+          href="https://github.com/AngelaMei"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/GithubLogo.svg"
+            alt="Github icon"
+            width={28}
+            height={28}
+          />
+        </a>
+        <a
+          className="hidden sm:block"
+          href="/docs/cv.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/CV.svg"
+            alt="CV icon"
+            width={28}
+            height={28}
+          />
+        </a>
+      </div>
+      <a
+        className="flex items-center hover:underline hover:underline-offset-4"
+        href="/about"
+      >
+        About
+      </a>
+      <a
+        className="flex items-center hover:underline hover:underline-offset-4"
+        href="/works"
+      >
+        All Works
+      </a>
+    </header>
+  );
+}
+
