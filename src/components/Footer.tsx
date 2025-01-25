4607c391a86dcce9ae4f8ea072356a7c3a5731d8
Paul Wang <tzupengwang@gmail.com>
2025-01-25 19:04:47 +0100

    Move project metadata to src/data/projects.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import projects from "@/data/projects";
+
 export default function Footer() {
   return (
     <footer className="border-t-[1px] border-solid border-slate-300 flex flex-wrap gap-x-64 gap-y-16 items-center justify-center font-sans py-24">
@@ -15,7 +17,7 @@ export default function Footer() {
         Mei
       </div>
       <div className="grid grid-flow-row sm:grid-rows-[repeat(4,_auto)] sm:grid-flow-col gap-y-4 gap-x-20 auto-cols-max text-md sm:text-xl">
-        <span className="row-start-1 text-xl sm:text-3xl font-medium py-2">
+        <span className="text-xl sm:text-3xl font-medium py-2">
           Site Map
         </span>
         <a
@@ -36,45 +38,19 @@ export default function Footer() {
         >
           All Works
         </a>
-        <span className="sm:col-[2_/_4] text-xl sm:text-3xl font-medium py-2">
+        <span className="sm:row-start-1 sm:col-span-2 text-xl sm:text-3xl font-medium py-2">
           Selected Projects
         </span>
-        <a
-          className="sm:row-start-2 hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          AI Travel Assistant
-        </a>
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          Guide App
-        </a>
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          Gesture Recognition Pet
-        </a>
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          Student Social Life
-        </a>
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          Dairy Subscription Service
-        </a>
-        <a
-          className="hover:underline hover:underline-offset-4"
-          href="/"
-        >
-          Future Trading Application
-        </a>
+        {
+          projects.map(project => (
+            <a
+              className="hover:underline hover:underline-offset-4"
+              href={project.pageUrl}
+            >
+              {project.name}
+            </a>
+          ))
+        }
       </div>
     </footer>
   );
