404f7531b7cda77d2857a6a063f18c76e2d237aa
Paul Wang <tzupengwang@gmail.com>
2025-02-05 20:03:17 +0100

    Update Header bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const darkMode = useContext(DarkModeContext);
 
   return (
-    <header className={`absolute l-0 t-0 w-full flex justify-center z-50 ${darkMode ? 'text-white' : 'text-black'}`}>
+    <header className={`fixed l-0 t-0 w-full flex justify-center z-50 ${darkMode ? 'text-white' : 'text-black'}`}>
       <div className="flex px-4 sm:max-[1450px]:px-20 min-[1450px]:px-0 w-full min-[1450px]:w-7xl items-center text-sm sm:text-xl gap-4 sm:gap-7 h-24 my-0.5">
         <Link href="/">
           <span className="text-[2.5rem] select-none font-picasso">ang</span>
@@ -21,12 +21,14 @@ export default function Header() {
             about me.
           </span>
         </Link>
+        {/* TODO: re-enable this after the page is implemented
         <Link href="/works">
           <span className="hover:underline hover:underline-offset-4 font-serif">
             all works.
           </span>
         </Link>
-        <Link href="/docs/cv.pdf" target="_blank" rel="noopener noreferrer">
+        */}
+        <Link href="https://drive.google.com/file/d/17-CJW2dArsCYxxthyIYxR-XOy4KzTuFk/view" target="_blank" rel="noopener noreferrer">
           <span className="hover:underline hover:underline-offset-4 font-serif">
             résumé.
           </span>
