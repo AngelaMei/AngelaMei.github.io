e6a6d01297781c6f6ee3f7ac9d5aa1f39947a7ef
Paul Wang <tzupengwang@gmail.com>
2025-02-05 20:22:16 +0100

    Experiment with Header backdrop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,10 @@ export default function Header() {
   const darkMode = useContext(DarkModeContext);
 
   return (
-    <header className={`fixed l-0 t-0 w-full flex justify-center z-50 ${darkMode ? 'text-white' : 'text-black'}`}>
+    <header
+      className={`fixed l-0 t-0 w-full flex justify-center z-50 ${darkMode ? 'text-white' : 'text-black'} backdrop-blur-md`}
+      style={{ maskImage: "linear-gradient(to bottom, rgb(0 0 0 / 100%), rgb(0 0 0 / 100%) 80%, transparent)", }}
+    >
       <div className="flex px-4 sm:max-[1450px]:px-20 min-[1450px]:px-0 w-full min-[1450px]:w-7xl items-center text-sm sm:text-xl gap-4 sm:gap-7 h-24 my-0.5">
         <Link href="/">
           <span className="text-[2.5rem] select-none font-picasso">ang</span>
