abf9eb4033009d55d81c4cc78857fdc45247d77b
AngelaMei <54565258+AngelaMei@users.noreply.github.com>
2025-02-05 21:18:25 -0500

    add about page RWD

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@
 
 import type { FC } from 'react';
 import Image from 'next/image';
+import { BaseText } from '@/components/utilities';
 
 import image1 from '@/public/media/about/img1.png';
 import image2 from '@/public/media/about/img2.png';
@@ -18,36 +19,35 @@ import letterM from '@/public/media/homepage/angela/M.svg';
 import letterI from '@/public/media/homepage/angela/i.svg';
 import letterDot from '@/public/media/homepage/angela/dot.svg';
 
-
 const Page: FC = () => {
   return (
     <div className="relative flex w-full justify-center bg-dark-bg">
-      <main className="relative flex flex-col items-center pt-50 pb-24 w-full px-4 sm:px-20 gap-y-8 sm:gap-y-20 font-sans text-white">
+      <main className="relative flex flex-col items-center pt-30 sm:pt-50 pb-24 w-full px-4 sm:px-20 gap-y-8 sm:gap-y-20 font-sans text-white">
         <div className="">
           <div
-            className="sticky top-50 flex flex-col items-center max-w-7xl font-picasso z-10"
+            className="sticky top-30 sm:top-50 flex flex-col items-center max-w-7xl font-picasso z-10"
           >
-            <span className="text-center text-9xl">ANGELA MEI</span>
-            <span className="text-center text-8xl">
+            <span className="text-center text-7xl sm:text-9xl">ANGELA MEI</span>
+            <span className="text-center text-6xl sm:text-8xl sm:pt-10">
               LOVES <span className="text-[#D5501C]">DESIGN</span>,<br/>
               Takes <span className="text-[#DFAC2B]">Photos</span>, Plays<br/>
               <span className="text-[#648A5D]">Volleyball</span> And <span className="text-[#5B9EDC]">Travels</span>.
             </span>
           </div>
           <div className="relative h-440 z-40">
-            <Image src={image1} className="absolute top-30 left-1/20 w-50 h-auto" alt=""/>
-            <Image src={image5} className="absolute top-80 left-5/7 w-80 h-auto" alt=""/>
-            <Image src={image3} className="absolute top-150 left-1/5 w-80 h-auto" alt=""/>
-            <Image src={image2} className="absolute top-250 left-5/7 w-60 h-auto" alt=""/>
-            <Image src={image4} className="absolute top-350 left-1/6 w-60 h-auto" alt=""/>
+            <Image src={image1} className="absolute top-0 left-1/20 w-30 sm:top-30 sm:left-1/20 sm:w-50 h-auto" alt=""/>
+            <Image src={image5} className="absolute top-70 left-2/7 w-60 sm:top-80 sm:left-5/7 sm:w-80 h-auto" alt=""/>
+            <Image src={image3} className="absolute top-150 left-0 w-60 sm:top-150 sm:left-1/5 sm:w-80 h-auto" alt=""/>
+            <Image src={image2} className="absolute top-240 left-4/7 w-40 sm:top-250 sm:left-5/7 sm:w-60 h-auto" alt=""/>
+            <Image src={image4} className="absolute top-300 left-1 w-30 sm:top-350 sm:left-1/6 sm:w-60 h-auto" alt=""/>
 
           </div>
         </div>
         {/* About Me section*/}
-        <div className="flex items-start w-full max-w-6xl pt-20">
+        <div className="flex items-start w-full max-w-6xl pt-8 sm:pt-20">
           {/* Name banner */}
           <div
-            className="basis-2/5 flex flex-col items-start font-picasso leading-none select-none pointer-events-none"
+            className="hidden sm:flex basis-2/5 flex-col items-start font-picasso leading-none select-none pointer-events-none"
           >
             <div className="flex pb-7 justify-center">
               <Image className="h-13 w-auto pr-2" src={letterA} alt=""/>
@@ -64,14 +64,14 @@ const Page: FC = () => {
               <Image className="h-3 w-auto" src={letterDot} alt=""/>
             </div>
           </div>
-          <div className="basis-3/5 flex flex-col gap-y-8">
+          <div className="basis-full sm:basis-3/5 flex flex-col gap-y-8">
             <span className="text-2xl font-serif">about me.</span>
-            <span className="text-xl font-sans">
+            <BaseText>
               Given my name's Mandarin meaning is "unexpected", I always try my best to deliver work that is better than expected.
               <br/><br/>
               With a background in Finance and HCI, I bring a unique analytical perspective to design challenges.<br/><br/>
               Currently pursuing my Master's in Human-Computer Interaction at the University of Maryland, I strive to create digital experiences that are both innovative and practical.
-            </span>
+            </BaseText>
           </div>
         </div>
       </main>
