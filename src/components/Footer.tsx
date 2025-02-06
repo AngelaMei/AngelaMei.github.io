416de814e42557dc5c33607d4569d933de5e538f
AngelaMei <54565258+AngelaMei@users.noreply.github.com>
2025-02-05 21:18:59 -0500

    Modify Footer RWD, add icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useContext } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import projects from '@/data/projects';
 import { DarkModeContext } from '@/context';
@@ -10,8 +12,38 @@ export default function Footer() {
   const darkMode = useContext(DarkModeContext);
 
   return (
-    <footer className={`border-t-[1px] border-solid border-slate-300 flex flex-wrap gap-x-64 gap-y-16 items-center justify-center font-serif py-24 ${darkMode ? 'text-white bg-dark-bg' : 'text-black bg-white'}`}>
-      <span className="text-[2.5rem] select-none font-picasso">ang</span>
+    <footer className={`border-t-[1px] border-solid border-zinc-500 flex flex-wrap gap-x-64 gap-y-16 items-center justify-center font-serif py-24 ${darkMode ? 'text-white bg-dark-bg' : 'text-black bg-white'}`}>
+      <div>
+        <MagicLink href="/">
+          <span className="text-[2.5rem] select-none font-picasso">ang</span>
+        </MagicLink>
+        <div className="flex gap-x-4 pt-6 items-center">
+          <MagicLink href="mailto:angela101475@gmail.com">
+            <Image 
+              src="/icons/Envelope.svg" 
+              alt="Goal Icon" 
+              width={28} 
+              height={28} 
+            />
+          </MagicLink>
+          <Link href="https://github.com/AngelaMei" target="_blank" rel="noopener noreferrer">
+            <Image 
+              src="/icons/GithubLogo.svg" 
+              alt="Goal Icon" 
+              width={28} 
+              height={28} 
+            />
+          </Link>
+          <Link href="https://www.linkedin.com/in/angela-mei" target="_blank" rel="noopener noreferrer">
+            <Image 
+              src="/icons/LinkedinLogo.svg" 
+              alt="Goal Icon" 
+              width={28} 
+              height={28} 
+            />
+          </Link>
+        </div>
+      </div>
       <div className="grid grid-flow-row sm:grid-rows-[repeat(4,_auto)] sm:grid-flow-col gap-y-3 gap-x-20 auto-cols-max text-md sm:text-xl">
         <span className="text-xl sm:text-3xl font-medium py-2">
           site map.
@@ -28,6 +60,11 @@ export default function Footer() {
         >
           about me.
         </MagicLink>
+        <Link href="https://drive.google.com/file/d/17-CJW2dArsCYxxthyIYxR-XOy4KzTuFk/view" target="_blank" rel="noopener noreferrer">
+          <span className="hover:underline hover:underline-offset-4 font-serif">
+            résumé.
+          </span>
+        </Link>
         {/*
         <MagicLink
           className="hover:underline hover:underline-offset-4"
