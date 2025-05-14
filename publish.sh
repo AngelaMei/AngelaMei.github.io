238a72c291a096e7cd405b08c880d6e087bfaa61
Paul Wang <tzupengwang@gmail.com>
2025-05-14 10:10:57 +0800

    Remove all generated files before building in the publish script

--- publish.sh
+++ publish.sh
@@ -11,6 +11,30 @@ COMMIT_HASH=$1
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 TARGET_BRANCH=gh-pages
 
+# Remove all generated files (except for the blacklisted ones)
+BLACKLIST=(
+  ".git"
+  ".gitignore"
+  ".nojekyll"
+  "CNAME"
+  "publish.sh"
+)
+
+# iterate over all files in the current directory
+for file in *; do
+  # check if the file is not in the blacklist
+  if [[ ! " ${BLACKLIST[@]} " =~ " ${file} " ]]; then
+    # check if the file is a directory
+    if [ -d "$file" ]; then
+      echo "Removing directory: $file"
+      rm -rf "$file"
+    else
+      echo "Removing file: $file"
+      rm -f "$file"
+    fi
+  fi
+done
+
 echo "Current branch: $CURRENT_BRANCH"
 echo "Checking out commit: $COMMIT_HASH"
 
